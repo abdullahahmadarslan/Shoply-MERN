86def1107a91bdc4efb5bbff1055b67c47040fab
Abdullah Ahmad Arslan <abdullah.ahmad.arslan@gmail.com>
2025-02-04 16:08:17 +0500

    auth controller ,routes ,and model added

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// routes
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+// db connection
+import { connectDB } from "./lib/db.js";
+
+// loading environment variables
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	connectDB();
+})
