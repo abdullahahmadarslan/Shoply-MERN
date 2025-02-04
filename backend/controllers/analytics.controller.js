5b103bad0eeda57dfc44bf477406eb045a9ddce3
Abdullah Ahmad Arslan <abdullah.ahmad.arslan@gmail.com>
2025-02-05 01:10:32 +0500

    analytics routes and controllers added

---
+++ backend/controllers/analytics.controller.js
@@ -0,0 +1,79 @@
+import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
+
+// get all time data
+export const getAnalyticsData = async () => {
+	const totalUsers = await User.countDocuments();
+	const totalProducts = await Product.countDocuments();
+
+	const salesData = await Order.aggregate([
+		{
+			$group: {
+				_id: null, // it groups all documents together irrespective of any field,
+				totalSales: { $sum: 1 },
+				totalRevenue: { $sum: "$totalAmount" },
+			},
+		},
+	]);
+
+	const { totalSales, totalRevenue } = salesData[0] || { totalSales: 0, totalRevenue: 0 };
+
+	return {
+		users: totalUsers,
+		products: totalProducts,
+		totalSales,
+		totalRevenue,
+	};
+};
+
+// getting last 7 days data from today
+export const getDailySalesData = async (startDate, endDate) => {
+	try {
+		const dailySalesData = await Order.aggregate([
+			{
+				$match: {  //filter the documents based on the condition      
+					createdAt: {
+						$gte: startDate,
+						$lte: endDate,
+					},
+				},
+			},
+			{
+				$group: { //then grouping based on dates of different days
+					_id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+					sales: { $sum: 1 },
+					revenue: { $sum: "$totalAmount" },
+				},
+			},
+			{ $sort: { _id: 1 } }, //sorting the grouped data documents based on date as _id has date now
+		]);
+
+		const dateArray = getDatesInRange(startDate, endDate);
+		// console.log(dateArray) // ['2024-08-18', '2024-08-19', ... ]
+
+		return dateArray.map((date) => {
+			const foundData = dailySalesData.find((item) => item._id === date); //item._id is date
+
+			return {
+				date,
+				sales: foundData?.sales || 0,
+				revenue: foundData?.revenue || 0,
+			};
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
+function getDatesInRange(startDate, endDate) {
+	const dates = [];
+	let currentDate = new Date(startDate);
+
+	while (currentDate <= endDate) {
+		dates.push(currentDate.toISOString().split("T")[0]);
+		currentDate.setDate(currentDate.getDate() + 1);
+	}
+
+	return dates;
+}
