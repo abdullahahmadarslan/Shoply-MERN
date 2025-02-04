398d1a5e5e21e425cd4d1b7b86dff6b880d28338
Abdullah Ahmad Arslan <abdullah.ahmad.arslan@gmail.com>
2025-02-05 00:08:37 +0500

    cloudinary setup done

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -12,10 +12,12 @@ import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
 router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
+
+// admin protected routes
+router.get("/", protectRoute, adminRoute, getAllProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
 router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
